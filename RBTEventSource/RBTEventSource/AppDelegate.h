16ba413077f26ce298b3ba95f986b7bae93ca97b
Romain Bousquet <rom.bousqu@gmail.com>
2015-08-14 22:32:38 +0200

    Initial commit

---
+++ RBTEventSource/RBTEventSource/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RBTEventSource
+//
+//  Created by Romain Bousquet on 14/08/2015.
+//  Copyright (c) 2015 Romain Bousquet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RBTEventSource/RBTEventSource/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RBTEventSource
+//
+//  Created by Romain Bousquet on 14/08/2015.
+//  Copyright (c) 2015 Romain Bousquet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
