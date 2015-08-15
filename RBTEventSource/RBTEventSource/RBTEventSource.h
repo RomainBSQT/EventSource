c43eeddbc1679f63233bbb6335b9ee50929ee278
Romain Bousquet <rom.bousqu@gmail.com>
2015-08-15 15:45:24 +0200

    Ordering files

---
+++ RBTEventSource/RBTEventSource/RBTEventSource.h
@@ -0,0 +1,13 @@
+//
+//  RBTEventSource.h
+//  RBTEventSource
+//
+//  Created by Romain Bousquet on 14/08/2015.
+//  Copyright (c) 2015 Romain Bousquet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RBTEventSource : NSObject
+
+@end
---
+++ RBTEventSource/RBTEventSource/RBTEventSource.m
@@ -0,0 +1,13 @@
+//
+//  RBTEventSource.m
+//  RBTEventSource
+//
+//  Created by Romain Bousquet on 14/08/2015.
+//  Copyright (c) 2015 Romain Bousquet. All rights reserved.
+//
+
+#import "RBTEventSource.h"
+
+@implementation RBTEventSource
+
+@end
